d14a0156ce8e0675a77a65ef27160845a4462f38
Jonas Hofer <jonas.hofer@outlook.com>
2014-03-15 16:27:44 +0100

    added fileexstension

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -1,13 +1,13 @@
 'use strict';
 
 angular.module('testprojektApp')
-  .controller('AboutCtrl', function ($scope, $http) {
+.controller('AboutCtrl', function ($scope, $http) {
 
 
-$scope.abouts = ["sali ich bin de jonas und ich mach do top modärni sache. yay","und nomel eis", "es tuet. OLE"]
+	$scope.abouts = ["sali ich bin de jonas und ich mach do top modärni sache. yay","und nomel eis", "es tuet. OLE"]
 
 
-    $http.get('/api/awesomeThings').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
-  });
+	$http.get('/about/list').success(function(abouts) {
+		$scope.abouts = abouts;
+	});
+});
