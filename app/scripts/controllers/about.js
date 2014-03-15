bfe24d12ed046f0ca778b78f73e9e9500f064624
Jonas Hofer <jonas.hofer@outlook.com>
2014-03-15 15:58:32 +0100

    about added

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -4,10 +4,10 @@ angular.module('testprojektApp')
   .controller('AboutCtrl', function ($scope, $http) {
 
 
-$scope.text = "sali ich bin de jonas und ich mach do top modärni sache. yay"
+$scope.abouts = ["sali ich bin de jonas und ich mach do top modärni sache. yay","und nomel eis", "es tuet. OLE"]
 
 
-    // $http.get('/api/awesomeThings').success(function(awesomeThings) {
-    //   $scope.awesomeThings = awesomeThings;
-    // });
+    $http.get('/api/awesomeThings').success(function(awesomeThings) {
+      $scope.awesomeThings = awesomeThings;
+    });
   });
